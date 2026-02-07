7d4db5e91d5e451c818bc18f3ce86931c070ff29
JaehyunK <kk2007pp@gmail.com>
2026-02-07 11:46:35 +0900

    Eruda 제거

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,10 +6,6 @@ import VerifyWrapper from "../components/VerifyWrapper";
 export default async function RootLayout({ children }) {
   return (
     <html lang="ko">
-      <head>
-        <script src="https://cdn.jsdelivr.net/npm/eruda"></script>
-        <script>eruda.init();</script>
-      </head>
       <body className="app-body">
         <MiniKitProvider props={{ appId: process.env.APP_ID }}>
           <AuthWrapper>
